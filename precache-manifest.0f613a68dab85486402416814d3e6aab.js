65269e6e852fc671b1c42b2efea124baa546faa8
SantasDev <krishenaosanta@hotmail.com>
2020-02-26 20:33:30 -0500

    Updates

---
+++ precache-manifest.0f613a68dab85486402416814d3e6aab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d0807540514035d0146685d1b3277fe",
+    "url": "/fecofa-match-frontend/index.html"
+  },
+  {
+    "revision": "a89740473664863aaa6f",
+    "url": "/fecofa-match-frontend/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f25a54718fa85cf12ae6",
+    "url": "/fecofa-match-frontend/static/js/2.b6250ef2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/fecofa-match-frontend/static/js/2.b6250ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a89740473664863aaa6f",
+    "url": "/fecofa-match-frontend/static/js/main.a678ab70.chunk.js"
+  },
+  {
+    "revision": "ae2d0f2e6996664868d1",
+    "url": "/fecofa-match-frontend/static/js/runtime-main.35a2aceb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fecofa-match-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);

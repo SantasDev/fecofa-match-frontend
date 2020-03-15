ed8a88c5f16e243e30e8aebad34e25ce82eed5b6
SantasDev <krishenaosanta@hotmail.com>
2020-03-15 13:35:21 -0500

    [feat] enhance navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 import FcLayoutHeader from './components/layouts/LayoutHeader';
 import FcLayoutNav from './components/layouts/LayoutNav';
@@ -7,12 +10,41 @@ import FcLayoutNav from './components/layouts/LayoutNav';
 import HomePage from './pages/Home';
 import TestPage from './pages/Test';
 
+library.add(fab, faCheckSquare, faCoffee)
+
+const FecofaMenu = [
+  {
+    id: 1,
+    label: 'inicio',
+    link: '/',
+    icon: ''
+  },
+  {
+    id: 2,
+    label: 'Juegos',
+    link: '/games',
+    icon: ''
+  },
+  {
+    id: 3,
+    label: 'Equipos',
+    link: '/team',
+    icon: ''
+  },
+  {
+    id: 1,
+    label: 'Jugadores',
+    link: '/players',
+    icon: ''
+  }
+];
+
 function App() {
   return (
     <Router>
       <div className="App">
         <div className="fc-content_navbar">
-          <FcLayoutNav />
+          <FcLayoutNav navigation={FecofaMenu}/>
         </div>
         <div className="fc-content">
           <FcLayoutHeader></FcLayoutHeader>
--- src/components/layouts/LayoutNav.js
+++ src/components/layouts/LayoutNav.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const LayoutNavBar=()=>(
+const LayoutNavBar=(props)=>(
     <nav className="fc-navbar">
         <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/test">Test</Link>
-            </li>
+            {props.navigation.map(item=>(
+                <li>
+                    <Link to={ item.link }>
+                        <FontAwesomeIcon icon="check-square" size="2x" />
+                        {item.label}
+                    </Link>
+                </li>
+            ))}
         </ul>
     </nav>
 );

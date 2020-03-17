391f35cda1c0234d997868274fe6a77a6639a529
Cristian Henao <chenao@playvox.com>
2020-03-16 21:11:07 -0500

    [feat] add section navigation

--- src/components/layouts/LayoutHeader.js
+++ src/components/layouts/LayoutHeader.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import SectionNavigation from './SectionNavigation';
 
 const Layout = ()=>(
-    <header>FECOFA</header>
+    <header className="fc_header" >
+        <h1 className="fc_title" > FECOFA </h1>
+        <SectionNavigation />
+    </header>
 );
 
 export default Layout;

0bba57890ebee8498a69f74caf08231bfdbab18a
Cristian Henao <chenao@playvox.com>
2020-04-05 20:22:34 -0500

    [enhance] header component

--- src/components/layouts/LayoutHeader.js
+++ src/components/layouts/LayoutHeader.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SectionNavigation from './SectionNavigation';
 
-const LayoutHeader = (prop )=>(
-    <header className="fc_header" >
-        <h1 className="fc_title" > {prop.page.label} </h1>
-        <SectionNavigation sections={ prop.page.sections }/>
-    </header>
-);
+const LayoutHeader = (prop )=>{
+    return (
+        <header className="fc_header" >
+            <h1 className="fc_title" > {prop.title} </h1>
+            <SectionNavigation sections={ prop.sections }/>
+        </header>
+    );
+};
 
 export default LayoutHeader;

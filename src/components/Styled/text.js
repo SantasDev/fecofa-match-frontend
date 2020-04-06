04b8199be7ab9e910e3375d71454ff706067419b
Cristian Henao <chenao@playvox.com>
2020-04-05 20:23:41 -0500

    [feat] create transversal styled components

---
+++ src/components/Styled/text.js
@@ -0,0 +1,12 @@
+import Styled from 'styled-components';
+
+export const FcH1 = Styled.h1`
+  color: white;
+`; 
+
+
+export const DivCentered = Styled.div`
+  display: flex;
+  align-item: center;
+  justify-content: center;
+`;

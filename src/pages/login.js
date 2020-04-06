51409527d11b2ddd68e0c4fc8aaa5f75399ddbf6
Cristian Henao <chenao@playvox.com>
2020-04-05 20:23:15 -0500

    [feat] create login page

---
+++ src/pages/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Login extends React.Component(){
+  constructor(){
+    super();
+    this.state = {
+      user: '',
+      password: ''
+    }
+  }
+  render() {
+    return (
+      <div> </div>
+    );
+  }
+}
